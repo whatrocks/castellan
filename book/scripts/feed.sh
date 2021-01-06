12468b0db2bbed508cb699ab08d5bdc80932e426
Charlie Harrington <charles.harrington@gmail.com>
2021-01-06 08:00:03 -0800

    add book mp3s

---
+++ book/scripts/feed.sh
@@ -0,0 +1,68 @@
+# edit these to your liking
+PODCAST_TITLE="Castellan - Frankenstein"
+CATEGORY="TODO LOOK THIS UP"
+GENERATOR="Castellan"
+
+# automatic
+CURRENT_DATE="TODO IN UTC STRING"
+
+"""
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+  <channel>
+    <title>$PODCAST_TITLE</title>
+    <link>https://youtube.com/playlist?list=PLC_pWofH2gkvxyuQq51FpXjDAF0mxFEVw</link>
+    <description>$PODCAST_TITLE (2020-12-28 19:48:38 +0000 UTC)</description>
+    <category>$CATEGORY</category>
+    <generator>$GENERATOR</generator>
+    <language>en-us</language>
+    <lastBuildDate>Wed, 06 Jan 2021 15:36:27 +0000</lastBuildDate>
+    <pubDate>Mon, 28 Dec 2020 19:48:38 +0000</pubDate>
+    <image>
+      <url>https://i.ytimg.com/vi/P380D7yX8xQ/default.jpg</url>
+      <title>$PODCAST_TITLE</title>
+      <link>https://youtube.com/playlist?list=PLC_pWofH2gkvxyuQq51FpXjDAF0mxFEVw</link>
+    </image>
+    <itunes:author>$PODCAST_TITLE</itunes:author>
+    <itunes:subtitle>$PODCAST_TITLE</itunes:subtitle>
+    <itunes:summary><![CDATA[$PODCAST_TITLE (2020-12-28 19:48:38 +0000 UTC)]]></itunes:summary>
+    <itunes:image href="https://i.ytimg.com/vi/P380D7yX8xQ/default.jpg"></itunes:image>
+    <itunes:explicit>no</itunes:explicit>
+    <itunes:category text="$CATEGORY"></itunes:category>
+    <item>
+      <guid>ji5_MqicxSo</guid>
+      <title>Randy Pausch Last Lecture: Achieving Your Childhood Dreams</title>
+      <link>https://youtube.com/watch?v=ji5_MqicxSo</link>
+      <description>Carnegie Mellon Professor Randy Pausch (Oct. 23, 1960 - July 25, 2008) gave his last lecture at the university Sept. 18, 2007, before a packed McConomy Auditorium. In his moving presentation, &#34;Really Achieving Your Childhood Dreams,&#34; Pausch talked about his lessons learned and gave advice to students on how to achieve their own career and personal goals. &#xD;&#xA;&#xD;&#xA;For more on Randy, visit: http://www.cmu.edu/randyslecture&#xD;&#xA;&#xD;&#xA;Learn how to support the Randy Pausch Memorial Bridge, visit: http://www.cmu.edu/homepage/images/extras/emails/pausch/pausch_bridge.html</description>
+      <pubDate>Wed, 06 Jan 2021 06:21:09 +0000</pubDate>
+      <enclosure url="https://rockswhat.github.io/listener/listen_laterz/ji5_MqicxSo.mp3" length="44219421" type="audio/mpeg"></enclosure>
+      <itunes:author>$PODCAST_TITLE</itunes:author>
+      <itunes:subtitle>Randy Pausch Last Lecture: Achieving Your Childhood Dreams</itunes:subtitle>
+      <itunes:summary><![CDATA[Carnegie Mellon Professor Randy Pausch (Oct. 23, 1960 - July 25, 2008) gave his last lecture at the university Sept. 18, 2007, before a packed McConomy Auditorium. In his moving presentation, "Really Achieving Your Childhood Dreams," Pausch talked about his lessons learned and gave advice to students on how to achieve their own career and personal goals. 
+For more on Randy, visit: http://www.cmu.edu/randyslecture
+Learn how to support the Randy Pausch Memorial Bridge, visit: http://www.cmu.edu/homepage/images/extras/emails/pausch/pausch_bridge.html]]></itunes:summary>
+      <itunes:image href="https://i.ytimg.com/vi/ji5_MqicxSo/default.jpg"></itunes:image>
+      <itunes:duration>1:16:27</itunes:duration>
+      <itunes:explicit>no</itunes:explicit>
+      <itunes:order>1</itunes:order>
+    </item>
+    <item>
+      <guid>a1zDuOPkMSw</guid>
+      <title>Hamming, &#34;You and Your Research&#34; (June 6, 1995)</title>
+      <link>https://youtube.com/watch?v=a1zDuOPkMSw</link>
+      <description>Intro: I have given a talk with this title many times, and it turns out from discussions after the talk I could have just as well have called it &#34;You and Your Engineering Career,&#34; or even &#34;You and Your Career.&#34; But I left the word &#34;Research&#34; in the title because that is what I have most studied. From the previous chapters you have an adequate background for how I made the study, and I need not mention again the names of the famous people I have studied closely. The earlier chapters are, in sense, just a great expansion, with much more detail, of the original talk. This chapter is, in a sense, a summary of the previous chapters.&#xA;&#xA;&#xA;&#xA;The Art of Doing Science and Engineering: Learning to Learn&#34; was the capstone course by Dr. Richard W. Hamming (1915-1998) for graduate students at the Naval Postgraduate School (NPS) in Monterey California. &#xA;&#xA;This course is intended to instill a &#34;style of thinking&#34; that will enhance one&#39;s ability to function as a problem solver of complex technical issues. With respect, students sometimes called the course &#34;Hamming on Hamming&#34; because he relates many research collaborations, discoveries, inventions and achievements of his own. This collection of stories and carefully distilled insights relates how those discoveries came about. Most importantly, these presentations provide objective analysis about the thought processes and reasoning that took place as Dr. Hamming, his associates and other major thinkers, in computer science and electronics, progressed through the grand challenges of science and engineering in the twentieth century.</description>
+      <pubDate>Wed, 06 Jan 2021 05:50:07 +0000</pubDate>
+      <enclosure url="https://rockswhat.github.io/listener/listen_laterz/a1zDuOPkMSw.mp3" length="25044093" type="audio/mpeg"></enclosure>
+      <itunes:author>$PODCAST_TITLE</itunes:author>
+      <itunes:subtitle>Hamming, &#34;You and Your Research&#34; (June 6, 1995)</itunes:subtitle>
+      <itunes:summary><![CDATA[Intro: I have given a talk with this title many times, and it turns out from discussions after the talk I could have just as well have called it "You and Your Engineering Career," or even "You and Your Career." But I left the word "Research" in the title because that is what I have most studied. From the previous chapters you have an adequate background for how I made the study, and I need not mention again the names of the famous people I have studied closely. The earlier chapters are, in sense, just a great expansion, with much more detail, of the original talk. This chapter is, in a sense, a summary of the previous chapters.
+The Art of Doing Science and Engineering: Learning to Learn" was the capstone course by Dr. Richard W. Hamming (1915-1998) for graduate students at the Naval Postgraduate School (NPS) in Monterey California. 
+This course is intended to instill a "style of thinking" that will enhance one's ability to function as a problem solver of complex technical issues. With respect, students sometimes called the course "Hamming on Hamming" because he relates many research collaborations, discoveries, inventions and achievements of his own. This collection of stories and carefully distilled insights relates how those discoveries came about. Most importantly, these presentations provide objective analysis about the thought processes and reasoning that took place as Dr. Hamming, his associates and other major thinkers, in computer science and electronics, progressed through the grand challenges of science and engineering in the twentieth century.]]></itunes:summary>
+      <itunes:image href="https://i.ytimg.com/vi/a1zDuOPkMSw/default.jpg"></itunes:image>
+      <itunes:duration>44:03</itunes:duration>
+      <itunes:explicit>no</itunes:explicit>
+      <itunes:order>3</itunes:order>
+    </item>
+  </channel>
+</rss>
+"""
