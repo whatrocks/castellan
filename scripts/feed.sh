93d85cf683c09737fd1c2707276ca139947140f6
Charlie Harrington <charles.harrington@gmail.com>
2021-01-07 12:05:50 -0800

    again

--- scripts/feed.sh
+++ scripts/feed.sh
@@ -6,7 +6,7 @@ PODCAST_AUTHOR="Castellan"
 CATEGORY="Technology"
 GENERATOR="Castellan"
 LINK="https://whatrocks.github.io/castellan/"
-IMG="https://whatrocks.github.io/castellan/f3.png"
+IMG="https://whatrocks.github.io/castellan/f4.png"
 
 # automatic
 CURRENT_DATE=$(date -R)
